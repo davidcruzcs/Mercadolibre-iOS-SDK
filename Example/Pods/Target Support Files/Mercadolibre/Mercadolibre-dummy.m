335f0ad148aa85a07ee4020363e7bd64d545a7f6
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2016-08-28 08:35:56 -0500

    Initial Commit

---
+++ Example/Pods/Target Support Files/Mercadolibre/Mercadolibre-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Mercadolibre : NSObject
+@end
+@implementation PodsDummy_Mercadolibre
+@end
---
+++ Example/Pods/Target Support Files/Mercadolibre/Mercadolibre-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MercadolibreVersionNumber;
+FOUNDATION_EXPORT const unsigned char MercadolibreVersionString[];
+
